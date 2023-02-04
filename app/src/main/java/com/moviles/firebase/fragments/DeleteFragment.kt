fe181a921df289e17f024982005a6d6be0f0c6ef
Angiey1 <angiey1@hotmail.com>
2023-02-04 00:44:56 -0500

    Conf Firebase y Delete
    Se incluye archivo de configuración, buildScript, dependencias y plugin
    Código para delete un usuario
    Se incluyen comentarios del código

--- app/src/main/java/com/moviles/firebase/fragments/DeleteFragment.kt
+++ app/src/main/java/com/moviles/firebase/fragments/DeleteFragment.kt
@@ -5,56 +5,62 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.moviles.firebase.R
+import com.moviles.firebase.databinding.FragmentDeleteBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DeleteFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DeleteFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var binding: FragmentDeleteBinding
+    //DB
+    private lateinit var dataBase: DatabaseReference
+    
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_delete, container, false)
+        binding = FragmentDeleteBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+    
+    private fun init(){
+        //Escucha botón
+        binding.buttonDelete.setOnClickListener {
+            //Obtención de datos
+            val userName = binding.nameUser.text.toString()
+            //Verificar si es nulo o empty
+            if(userName.isNullOrEmpty()){
+                deleteUser(userName)
+            }else{
+                Toast.makeText(activity?.baseContext,
+                "Por favor rellene el campo de nombre de usuario",
+                Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DeleteFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DeleteFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun deleteUser(userName: String) {
+        //Instancia
+        dataBase = FirebaseDatabase.getInstance().getReference("Users")
+        dataBase.child(userName).removeValue().addOnSuccessListener {
+            //Eliminar valores de campo
+            binding.nameUser.text?.clear()
+            //Mensaje
+            Toast.makeText(activity?.baseContext,
+                "Usuario eliminado exitosamente",
+                Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener{
+            Toast.makeText(activity?.baseContext,
+                "Falla al tratar de eliminar al usuario",
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        init()
     }
 }
