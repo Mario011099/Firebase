8c821dea1b4d1570be8e61a50062d2f2cdd24a64
Mario011099 <mario.sanchez@grupoasinfo.com>
2023-02-04 00:07:39 -0500

    Se añade id a elementos de fragment y se implementa obtención de datos desde firebase

--- app/src/main/java/com/moviles/firebase/fragments/SearchFragment.kt
+++ app/src/main/java/com/moviles/firebase/fragments/SearchFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.moviles.firebase.R
+import com.moviles.firebase.databinding.FragmentSearchBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,43 +20,45 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentSearchBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        binding.buttonLogin.setOnClickListener {
+            val userName: String = binding.userNameField.text.toString()
+            if (userName.isNotEmpty()) {
+                readData(userName)
+            } else {
+                Toast.makeText(context, "Please enter Username", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun readData(userName: String) {
+        database = FirebaseDatabase.getInstance().getReference("Users")
+        databae.child(userName).get().addOnSuccessListener {
+
+            if (it.exists()) {
+                val firstName = it.child("firstName").value
+                val lastName = it.child("lastName").value
+                val age = it.child("age").value
+                Toast.makeText(context, "Successfuly Read", Toast.LENGTH_SHORT).show()
+                binding.userNameField.text?.clear()
+                binding.name.text = firstName.toString()
+                binding.lastName.text = lastName.toString()
+                binding.age.text = age.toString()
+
+            } else {
+                Toast.makeText(context, "Username doesn't exist", Toast.LENGTH_SHORT).show()
             }
+
+        }.addOnFailureListener {
+            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show()
+        }
     }
 }
